b5ce82ccc2db6baf3720d0c93ec1e692ff5026ae
mao <1296193245@qq.com>
2022-09-28 13:42:26 +0800

    android_Room_frame

---
+++ app/src/main/java/mao/android_room_frame/dao/StudentDao.java
@@ -0,0 +1,88 @@
+package mao.android_room_frame.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import mao.android_room_frame.entity.Student;
+
+/**
+ * Project name(项目名称)：android_Room_frame
+ * Package(包名): mao.android_room_frame.dao
+ * Interface(接口名): StudentDao
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/9/28
+ * Time(创建时间)： 13:31
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Dao
+public interface StudentDao
+{
+    /**
+     * 查询所有
+     *
+     * @return {@link List}<{@link Student}>
+     */
+    @Query("select * from student")
+    List<Student> queryAll();
+
+    /**
+     * 查询
+     *
+     * @param id id
+     * @return {@link Student}
+     */
+    @Query("select * from student where id=:id")
+    Student query(Long id);
+
+    /**
+     * 插入
+     *
+     * @param student 学生
+     * @return boolean
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    boolean insert(Student student);
+
+    /**
+     * 插入列表
+     *
+     * @param list 列表
+     */
+    @Insert
+    void insertList(List<Student> list);
+
+    /**
+     * 更新
+     *
+     * @param student 学生
+     * @return boolean
+     */
+    @Update
+    boolean update(Student student);
+
+    /**
+     * 删除
+     *
+     * @param student 学生
+     * @return boolean
+     */
+    @Delete
+    boolean delete(Student student);
+
+    /**
+     * 删除所有
+     */
+    @Query("delete from Student where 1=1")
+    void deleteAll();
+
+}
---
+++ app/src/main/java/mao/android_room_frame/entity/Student.java
@@ -0,0 +1,201 @@
+package mao.android_room_frame.entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+/**
+ * Project name(项目名称)：android_Room_frame
+ * Package(包名): mao.android_room_frame.entity
+ * Class(类名): Student
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/9/28
+ * Time(创建时间)： 13:25
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Entity
+public class Student implements Serializable
+{
+    /**
+     * id
+     */
+    @PrimaryKey
+    @NonNull
+    private Long id;
+
+    /**
+     * 名字
+     */
+    private String name;
+
+    /**
+     * 性别
+     */
+    private String sex;
+
+    /**
+     * 年龄
+     */
+    private int age;
+
+    /**
+     * Instantiates a new Student.
+     */
+    public Student()
+    {
+
+    }
+
+    /**
+     * Instantiates a new Student.
+     *
+     * @param id   the id
+     * @param name the name
+     * @param sex  the sex
+     * @param age  the age
+     */
+    public Student(Long id, String name, String sex, int age)
+    {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public Long getId()
+    {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * Gets sex.
+     *
+     * @return the sex
+     */
+    public String getSex()
+    {
+        return sex;
+    }
+
+    /**
+     * Sets sex.
+     *
+     * @param sex the sex
+     */
+    public void setSex(String sex)
+    {
+        this.sex = sex;
+    }
+
+    /**
+     * Gets age.
+     *
+     * @return the age
+     */
+    public int getAge()
+    {
+        return age;
+    }
+
+    /**
+     * Sets age.
+     *
+     * @param age the age
+     */
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        Student student = (Student) o;
+
+        if (getAge() != student.getAge())
+        {
+            return false;
+        }
+        if (getId() != null ? !getId().equals(student.getId()) : student.getId() != null)
+        {
+            return false;
+        }
+        if (getName() != null ? !getName().equals(student.getName()) : student.getName() != null)
+        {
+            return false;
+        }
+        return getSex() != null ? getSex().equals(student.getSex()) : student.getSex() == null;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getSex() != null ? getSex().hashCode() : 0);
+        result = 31 * result + getAge();
+        return result;
+    }
+
+    @Override
+    @SuppressWarnings("all")
+    public String toString()
+    {
+        final StringBuilder stringbuilder = new StringBuilder();
+        stringbuilder.append("id：").append(id).append('\n');
+        stringbuilder.append("name：").append(name).append('\n');
+        stringbuilder.append("sex：").append(sex).append('\n');
+        stringbuilder.append("age：").append(age).append('\n');
+        return stringbuilder.toString();
+    }
+}
