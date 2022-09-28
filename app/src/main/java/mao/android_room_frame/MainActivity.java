7fd4bb0d598e69e3ee861d2884aed3fa4dc8dc80
mao <1296193245@qq.com>
2022-09-28 19:44:38 +0800

    android_Room_frame

--- app/src/main/java/mao/android_room_frame/MainActivity.java
+++ app/src/main/java/mao/android_room_frame/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import mao.android_room_frame.application.MainApplication;
+import mao.android_room_frame.dao.StudentDao;
+
 public class MainActivity extends AppCompatActivity
 {
+
+    private StudentDao studentDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        studentDao = MainApplication.getInstance().getStudentDatabase().getStudentDao();
     }
+
+
 }
---
+++ app/src/test/java/mao/android_room_frame/dao/StudentDaoTest.java
@@ -0,0 +1,15 @@
+package mao.android_room_frame.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentDaoTest
+{
+
+    @Test
+    public void queryAll()
+    {
+
+    }
+}
